9ef96c68a930dc12a860609e66afc96dd70c88dd
aelna354 <elnasrianconcept@gmail.com>
2018-07-30 06:56:53 -0600

    Init commit

---
+++ script.js
@@ -0,0 +1,59 @@
+$(document).ready(function (){
+	setUpTiles()
+});
+
+function Tile(row, col)
+{
+	this.row = row
+	this.col = col
+	this.piecekind = null
+	this.piececolor = null
+	this.highlighted = false
+	this.sourcemark = false
+	this.color = "grey"
+	if ((row + col) % 2 == 0)
+		this.color = "white"
+
+	this.html = function()
+	{
+		string = "<img src='images/"
+		if (this.piecekind==null)
+			string += this.color + "blank"
+		else
+			string += this.piececolor + this.piecekind
+		string += ".png' class='tile "+this.color+"'>"
+		return string
+	}
+
+	this.clear = function()
+	{
+		this.piecekind = null
+		this.piececolor = null
+		return this.html()
+	}
+
+	this.placePiece = function(kind, color)
+	{
+		this.piecekind = kind
+		this.piececolor = color
+		return this.html()
+	}
+}
+
+function setUpTiles()
+{
+	tiles = {}
+	for (i = 1; i < 9; i++)
+	{
+		$("#board").append("<div id='row"+i+"'></div>")
+		for (j = 1; j < 9; j++)
+		{
+			tiles[(i, j)] = new Tile(i, j)
+			b = tiles[(i, j)].html()
+			console.log(b)
+			$("#row"+i).append(b)
+		}
+	}	
+}
+
+
